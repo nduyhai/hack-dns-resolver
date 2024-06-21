29c3645933437a97eef194a59d8f904b0fb8f93d
ndhai <haind.vng@gmail.com>
2024-06-21 14:20:21 +0700

    feat: init project

---
+++ aggregate-service/src/test/java/com/nduyhai/aggregate/AggregateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nduyhai.aggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
