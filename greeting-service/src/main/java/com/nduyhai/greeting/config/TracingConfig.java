e7415ea6070f1d2b144705b8e4f931d378f170f7
ndhai <haind.vng@gmail.com>
2024-06-21 20:10:01 +0700

    chore: add missing annotation

--- greeting-service/src/main/java/com/nduyhai/greeting/config/TracingConfig.java
+++ greeting-service/src/main/java/com/nduyhai/greeting/config/TracingConfig.java
@@ -3,12 +3,14 @@ package com.nduyhai.greeting.config;
 import brave.Tracing;
 import brave.grpc.GrpcTracing;
 import io.grpc.ServerInterceptor;
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TracingConfig {
     @Bean
+    @GRpcGlobalInterceptor
     public ServerInterceptor tracingServerInterceptor(Tracing tracing) {
         return GrpcTracing.create(tracing).newServerInterceptor();
     }
