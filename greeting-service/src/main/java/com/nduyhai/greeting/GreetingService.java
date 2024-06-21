ad6759a04d33c776e47d038be304389011e17593
ndhai <haind.vng@gmail.com>
2024-06-21 15:18:10 +0700

    feat: add proto

---
+++ greeting-service/src/main/java/com/nduyhai/greeting/GreetingService.java
@@ -0,0 +1,28 @@
+package com.nduyhai.greeting;
+
+import com.nduyhai.grpc.Greeting;
+import com.nduyhai.grpc.GreetingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@GRpcService
+public class GreetingService extends GreetingServiceGrpc.GreetingServiceImplBase {
+    @Override
+    public void hello(Greeting.HelloRequest request,
+                      StreamObserver<Greeting.HelloResponse> responseObserver) {
+        try {
+            Greeting.HelloResponse response = Greeting.HelloResponse.newBuilder()
+                    .setGreeting("Hello, " + request.getName()).build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+            log.info("On completed");
+        } catch (Exception ex) {
+            responseObserver.onError(ex);
+        }
+    }
+}
